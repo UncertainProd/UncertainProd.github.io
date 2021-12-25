29e9106ee7fffe54dacfc69bb9cb588c90d8a0d0
UncertainProd <ucprod@github.com>
2021-12-26 04:55:46 +0530

    Added plotting and average offset

--- rhythmoffsettest/scripts.js
+++ rhythmoffsettest/scripts.js
@@ -3,25 +3,69 @@ const squares = document.querySelectorAll(".flashy-box");
 const startbtn = document.getElementById("start-test");
 const readyno = document.getElementById("ready-no");
 const offsetspan = document.getElementById("offset-span");
-const beep = document.getElementById("beep");
+const avgoffsetspan = document.getElementById('avg-offset-span');
+
+// data stuffs
+let times = [];
+let offsets = [];
+let running_sum = 0;
+let running_count = 0;
+
 let count = 3;
 let mode = 0; // start
 let testid;
+let inittime = 0;
 let started = false;
 let starttime = 0;
 const timeinterval = 500; // in ms
-let soundid;
+
+// some color stuff
+// best:(3, 219, 252) to worst:(252, 61, 3)
+let rslope = 249*2/timeinterval; // 252 - 3
+let r_initval = 252;
+
+let gslope = -158*2/timeinterval; // 61 - 219
+let g_initval = 61;
+
+let bslope = -249*2/timeinterval; // 3 - 252
+let b_initval = 3;
+function offsetToColor(offset_val) {
+    let getColorComponent = (slope, initval, x) => slope*(x - (timeinterval/2.0)) + initval;
+    return { r: getColorComponent(rslope, r_initval, offset_val), g: getColorComponent(gslope, g_initval, offset_val), b: getColorComponent(bslope, b_initval, offset_val) };
+}
+
+
+const plot_div = document.getElementById('timing-plot-div');
 
 startbtn.addEventListener("click", ()=>{
     if (mode === 0) {
+        // reset plots
+        let plot_config = {
+            x: times,
+            y: offsets,
+            mode: 'markers',
+            type: 'scatter',
+            marker: {
+                color: []
+            }
+        };
+        let layout_config = {
+            yaxis: {
+                range: [-(timeinterval/2) - 1, (timeinterval/2) + 1]
+            }
+        };
+        Plotly.newPlot( plot_div, [plot_config], layout_config, { responsive:true });
+
         const id = setInterval(()=>{ 
             if (count === 0) {
                 readyno.style.color = "green";
                 readyno.style.fontWeight = "bold";
                 readyno.innerText = "Go!";
+                avgoffsetspan.innerText = "_";
     
                 startbtn.innerText = "Reload";
                 startbtn.style.backgroundColor = "red";
+                inittime = Date.now();
                 mode = 1;
                 clearInterval(id);
                 startbtn.disabled = false;
@@ -39,7 +83,6 @@ startbtn.addEventListener("click", ()=>{
     else{
         // cleanup
         testid ? clearInterval(testid) : null;
-        soundid ? clearInterval(soundid) : null;
         for (const sq of squares) {
             sq.style.backgroundColor = "red";
         }
@@ -54,12 +97,13 @@ startbtn.addEventListener("click", ()=>{
         mode = 0;
         started = false;
         offsetspan.innerText = "_"
+        running_sum = 0;
+        running_count = 0;
     }
 });
 
 function runTest(){
     let boxindex = 0;
-    soundid = setInterval(()=>{beep.play()}, timeinterval);
     testid = setInterval(()=>{
         window.removeEventListener('keypress', timeKeyPress);
         boxindex > 0 ? squares[(boxindex-1)%4].style.backgroundColor = "red" : null;
@@ -75,6 +119,21 @@ function timeKeyPress(){
         console.log("KeyDown!");
         let currtime = Date.now();
         let diff = currtime - starttime;
-        offsetspan.innerText = ""+diff <= timeinterval/2.0 ? diff : diff - timeinterval;
+        let trueoffset = diff <= timeinterval/2.0 ? diff : diff - timeinterval;
+
+        running_sum += trueoffset;
+        running_count++;
+        
+        offsetspan.innerText = ""+(trueoffset);
+        avgoffsetspan.innerText = "" + (running_sum/running_count);
+        // do plotting stuff
+        let color = offsetToColor(Math.abs(trueoffset));
+        let update = {
+            x: [[(currtime - inittime)/1000]],
+            y: [[trueoffset]],
+            'marker.color': [[`rgb(${color.r}, ${color.g}, ${color.b})`]]
+        };
+        Plotly.extendTraces(plot_div, update, [0]);
+        window.removeEventListener('keypress', timeKeyPress);
     }
 }
